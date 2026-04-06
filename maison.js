73bbf6223a61a87b9dae359e57309abd066e824f
totobricolo38-source <totobricolo38@gmail.com>
2026-04-06 12:38:56 +0200

    up

--- maison.js
+++ maison.js
@@ -2,11 +2,29 @@
 
 const svgData = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 24">
-        <g fill="#00ffff" stroke="#00ffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M 4 21 L 4 11 L 10 5 L 16 11 L 16 21 Z" />
-            <path d="M 2 10 L 10 2 L 18 10" fill="none" />
-        </g>
-    </svg>`;
+    <g 
+        fill="#00ffff" 
+        stroke="#00ffff" 
+        stroke-width="1.5" 
+        stroke-linecap="round" 
+        stroke-linejoin="round"
+    >
+        <path d="M 4 21 L 4 11 L 10 5 L 16 11 L 16 21 Z" />
+
+        <path d="M 2 10 L 10 2 L 18 10" fill="none">
+            <animateTransform 
+                attributeName="transform" 
+                type="translate" 
+                values="0 0; 0 -1.2; 0 0" 
+                dur="2s" 
+                repeatCount="indefinite"
+                calcMode="spline" 
+                keySplines="0.4 0 0.6 1; 0.4 0 0.6 1"
+                keyTimes="0; 0.5; 1" 
+            />
+        </path>
+    </g>
+</svg>`;
 
 const imgMaison = new Image();
 const svgBlob = new Blob([svgData], {type: 'image/svg+xml;charset=utf-8'});
